318a55c6723ea771ec9a88fb8ae68cbed0663c24
OrlinGichev <o.gichev.genik@gmail.com>
2024-03-24 11:25:41 +0200

    Firebase project init

---
+++ BG-Papa/src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyAnclsO3g-FUHxSjUQu6--PKKogdJV6EGw",
+      authDomain: "bg-papa.firebaseapp.com",
+      projectId: "bg-papa",
+      storageBucket: "bg-papa.appspot.com",
+      messagingSenderId: "199505387036",
+      appId: "1:199505387036:web:d07294a698aa7912ee49d9"
+      }
+}
