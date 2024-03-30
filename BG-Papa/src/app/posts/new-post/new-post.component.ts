88f4f3f9214b3674a63bc2dd394719efd5e943eb
OrlinGichev <o.gichev.genik@gmail.com>
2024-03-30 12:19:44 +0200

    create function to save new post and reset form

--- BG-Papa/src/app/posts/new-post/new-post.component.ts
+++ BG-Papa/src/app/posts/new-post/new-post.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { addDoc, collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-new-post',
@@ -7,4 +9,47 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
 
+  @ViewChild("newPostForm") newPostForm: any;
+
+  firestore:Firestore = inject(Firestore);
+
+  savePost() : void {
+    const postCollection = collection(this.firestore, 'posts');
+    addDoc(postCollection, {
+      'title': this.newPostForm.value.title,
+      'category' : this.newPostForm.value.category,
+      'content': this.newPostForm.value.content
+    });
+  }
+
+  resetForm(): void {
+    this.newPostForm.reset({
+      'title': '',
+      'category' : '',
+      'content': ''
+    });
+  }
+
+  submitForm(): void {      
+      this.savePost();
+      this.resetForm();
+    }
 }
+
+
+//   resetUser(): void {
+//     this.registerForm.reset({
+//       'username': '',
+//       'email': '',
+//       'password': '',
+//       'confirm_password': '',
+//     });
+//   }
+
+//   submitForm() : void{
+//     alert(this.registerForm.value.username);
+//     this.saveUser();
+//     this.resetUser();
+//   }
+// }
+
