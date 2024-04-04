341810f6fcbcef22d7a76eeb5138bc7f2a06aa7d
OrlinGichev <o.gichev.genik@gmail.com>
2024-04-04 21:09:47 +0300

    function in ProfileComponent

--- BG-Papa/src/app/user/profile/profile.component.ts
+++ BG-Papa/src/app/user/profile/profile.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { UserForAuth } from 'src/app/types/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit  {
 
-  constructor(private userService: UserService){}
+  userData = {} as UserForAuth;
+  editedUserData : any = {};
+  isEditing : boolean = false;
 
-  name:string | undefined = this.userService.getUserKeyFromLocalStorage('username');
-  email:string | undefined = this.userService.getUserKeyFromLocalStorage('email');
-  interests:string | undefined = this.userService.getUserKeyFromLocalStorage('interests');
+  constructor(private userService: UserService, private router: Router){}
+
+  ngOnInit(): void {
+    const storeUserData = localStorage.getItem('currentUser');
+    if (storeUserData) {
+      this.userData = JSON.parse(storeUserData) ;
+      this.editedUserData = {...this.userData};
+    }
+  }
+
+  editProfile(): void {
+    this.isEditing = true;
+  }
+
+  saveChanges(): void {
+    this.userData = {...this.editedUserData};
+    this.isEditing = false;
+    localStorage.setItem('currentUser', JSON.stringify(this.userData));
+  }
+
+
+  // name:string | undefined = this.userService.getUserKeyFromLocalStorage('username');
+  // email:string | undefined = this.userService.getUserKeyFromLocalStorage('email');
+  // interests:string | undefined = this.userService.getUserKeyFromLocalStorage('interests');
 
   
 }
